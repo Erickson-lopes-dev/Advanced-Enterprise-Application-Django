a21f66ad046f5f388041dd583823a8c32c80b624
EricksonLopes <ofc.erickson@gmail.com>
2020-09-15 09:01:27 -0300

    criacao de apps

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,8 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.departamentos.models import Departamento
+from apps.empresas.models import Empresa
 
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
+
+    # relacionando com usuario de um pra um
+    User = models.OneToOneField(User, on_delete=models.PROTECT)
+
+    # O usuário pode estar relacionado a multiplos departamentos
+    departamentos = models.ManyToManyField(Departamento)
+
+    # Relacionando o usuário a uma empresa
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome
