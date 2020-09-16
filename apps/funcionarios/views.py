d72e09814abf46280ab752ba2cd7fcaf4126145c
EricksonLopes <ofc.erickson@gmail.com>
2020-09-16 07:00:03 -0300

    filtrando pela empresa logada

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -5,3 +5,9 @@ from apps.funcionarios.models import Funcionario
 
 class FuncionariosList(ListView):
     model = Funcionario
+
+    def get_queryset(self):
+        # pegar a empresa do usuario logago
+        empresa_logada = self.request.user.funcionario.empresa
+        # Filtrar funcionarios que tenha a mesma empresa do user logado
+        return Funcionario.objects.filter(empresa=empresa_logada)
