676e07c77a5edad3bfe11426e44d4cce81221395
EricksonLopes <ofc.erickson@gmail.com>
2020-09-16 10:27:30 -0300

    update delete

--- apps/departamentos/urls.py
+++ apps/departamentos/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from apps.departamentos.views import DepartamentosList
+from apps.departamentos.views import DepartamentosList, DepartamentosCreate, DepartamentosUpdate, DepartamentosDelete
 
 urlpatterns = [
     path('list', DepartamentosList.as_view(), name='list_departamentos'),
-    # path('novo/', DepartamentosList.as_view(), name='novo_departamentos'),
-    # path('update/<int:pk/>', DepartamentosList.as_view(), name='update_departamentos'),
-    # path('delete/<int:pk/>', DepartamentosList.as_view(), name='delete_departamentos'),
+    path('novo/', DepartamentosCreate.as_view(), name='novo_departamento'),
+    path('update/<int:pk>/', DepartamentosUpdate.as_view(), name='update_departamento'),
+    path('delete/<int:pk>/', DepartamentosDelete.as_view(), name='delete_departamento'),
 ]
--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from apps.departamentos.models import Departamento
 
@@ -10,3 +11,29 @@ class DepartamentosList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return Departamento.objects.filter(empresa=empresa_logada)
+
+
+class DepartamentosCreate(CreateView):
+    model = Departamento
+    fields = ['nome']
+
+    def form_valid(self, form):
+        departamento = form.save(commit=False)
+        # pegar a empresa em que o usuario logado esta registrado salvando o dado no campo
+        departamento.empresa = self.request.user.funcionario.empresa
+        departamento.save()
+        return super(DepartamentosCreate, self).form_valid(form)
+
+    success_url = reverse_lazy('list_departamentos')
+
+
+class DepartamentosUpdate(UpdateView):
+    model = Departamento
+    fields = ['nome']
+
+    success_url = reverse_lazy('list_departamentos')
+
+
+class DepartamentosDelete(DeleteView):
+    model = Departamento
+    success_url = reverse_lazy('list_departamentos')
